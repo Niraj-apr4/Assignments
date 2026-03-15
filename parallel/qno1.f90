3d04312600e85307a77a26b25e27de26abb6ffc3
niraj <niraj.apr4@gmail.com>
2026-03-15 18:15:31 +0530

    monte_carlo added

--- parallel/qno1.f90
+++ parallel/qno1.f90
@@ -1,12 +1,15 @@
 program monte_carlo
+    use omp_lib
     implicit none 
 
     ! variable declararions 
     integer, parameter :: N = 100000
+    integer :: nthreads 
     real(8) , dimension(N) :: x, y, z
     real(8) :: val , f_sum = 0.0d0 , f_sq_sum = 0.0d0 , f_avg, f_sq_avg , f_rms , vol
     real(8) :: ans
     integer :: i 
+    real(8) :: start, finish , rate
 
 
     ! N = no of random poins , larger N better approximations
@@ -17,18 +20,28 @@ program monte_carlo
     call random_number (z)
     z = z * 2 ! z value ranges from 0 to 2
 
-    do i = 1 , N
+    call cpu_time(start)
+    !$omp parallel private(val, i)
+    !$omp do reduction(+:f_sum, f_sq_sum)
+    do i = 1, N
         val = func(x(i), y(i), z(i))
         f_sum = f_sum + val
         f_sq_sum = f_sq_sum + val ** 2
     end do
+    !$omp end do
+    !$omp end parallel
+    call cpu_time(finish)
+
+
 
     f_avg = f_sum / N
     f_sq_avg = f_sq_sum / N
     f_rms = sqrt((f_sq_avg - f_avg **2)/N)
 
     vol = 4.0d0 * 3.0d0 * 2.0d0
-    print * ,"INTEGRAL = ", vol * f_avg , "+/-"  , f_rms * vol
+
+    print * ,"INTEGRAL = ", vol * f_avg , "+/-"  &
+    & , f_rms * vol ,"time ", (finish - start) * 1000, "ms"
 
 
     contains
